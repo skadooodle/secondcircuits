f4f8a75e9569d3790910b4c7583c93ad098ac4f3
skadoodle <30576133+skadooodle@users.noreply.github.com>
2025-02-17 10:14:55 -0500

    Formatting

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Second Circuits';
-export const SITE_DESCRIPTION = 'Sustainable resale and donation for electronics!';
+export const SITE_TITLE = "Second Circuits";
+export const SITE_DESCRIPTION = "Sustainable resale and donation for electronics!";
