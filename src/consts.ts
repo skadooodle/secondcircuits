706e47f77f1f87b13462a6e79342bf2a2093ccdc
skadoodle <30576133+skadooodle@users.noreply.github.com>
2025-02-17 10:12:36 -0500

    Description Edit

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Second Circuits';
-export const SITE_DESCRIPTION = 'Sustainable electronics resale and donation!';
+export const SITE_DESCRIPTION = 'Sustainable resale and donation for electronics!';
